6b9cd720a764845c7458061c817b708ef91aa9c8
Ndirangu Kariuki <ndirashjose@gmail.com>
2024-10-30 23:02:22 +0300

    (feat): add limit restriction

--- library_management/library_management/doctype/library_transaction/library_transaction.py
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -9,7 +9,8 @@ from frappe.model.docstatus import DocStatus
 class LibraryTransaction(Document):
 	def before_submit(self):
 		if self.type == 'Issue':
-			self.validate_Issue()
+			self.validate_issue()
+			self.validate_maximum_limit()
 			# set the article status to be issued
 			article = frappe.get_doc("Article", self.article)
 			article.status = "Issued"
@@ -35,13 +36,27 @@ class LibraryTransaction(Document):
 		if article.status == 'Available':
 			frappe.throw(f"Article cannot {article.name} be returned without bieng issued first")
 
+
+	def validate_maximum_limit(self):
+		max_articles = frappe.db.get_single_value("Library Settings", "max_articles")
+		count = frappe.db.count(
+			"Library Transaction",
+			{
+				"library_member": self.library_member,
+				"type": "Issue",
+				"docstatus": DocStatus.submitted()
+			},
+		)
+		if count >= max_articles:
+			frappe.throw("Maximum limit reach for issuing articles")
+			
 	def validate_membership(self):
 		# check if a valid membership exists for this library member
 		valid_membership = frappe.db.exists(
 			"Library Membership",
 			{
 				"library_member": self.library_member,
-				"docstatus": DocStatus.submited(),
+				"docstatus": DocStatus.submitted(),
 				"from_date": ("<", self.date),
 				"to_date": (">", self.date)
 			},
